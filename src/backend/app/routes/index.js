ea8ae4fc4fbc70a538d5d806937eac2ad4636783
Victor Baraka <barakavictor62@gmail.com>
2020-05-22 08:12:04 +0300

    Initial project scafolding

---
+++ src/backend/app/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  response.status(200).json(
+    { message: 'Hello World from Express' }
+    );
+});
+
+module.exports = router;
---
+++ src/backend/app/routes/users.js
@@ -0,0 +1,8 @@
+var router = require('express').Router();
+
+/* GET users listing. */
+router.get('/', function(_request, response, _next) {
+  response.status(200).json({message: 'respond with a resource'});
+});
+
+module.exports = router;
