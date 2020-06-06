e90ceadb58610898031bf4f4a3ac88a771572d0c
Victor Baraka <barakavictor62@gmail.com>
2020-06-06 16:36:05 +0300

    ft(login): implement login ui structure

--- src/app/components/auth/login/Login.js
+++ src/app/components/auth/login/Login.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css';
 
-const Login = () => (
-        <>
-        Login.......
-        </>
-);
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submitCredentials = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(`${username}@${password}`);
+  };
+
+  return (
+        <div className='login-form-wrapper'>
+            <form onSubmit = {(e) => submitCredentials(e)}>
+                <label htmlFor='username'>Username</label>
+                <input
+                    type='text'
+                    className='form-control'
+                    id='username'
+                    value = {username}
+                    onChange={(e) => setUsername(e.target.value)} />
+                <label htmlFor='password'>Password</label>
+                <input
+                    type='password'
+                    id='password'
+                    className='form-control'
+                    value = {password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button
+                    type='submit'
+                    className='btn btn-primary'
+                    disabled= {loading}>
+                        Login
+                </button>
+            </form>
+        </div>
+  );
+};
 
 export default Login;
