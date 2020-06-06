9b2b64ace1426ac655fb086c95cdc0f76624d9bc
Victor Baraka <barakavictor62@gmail.com>
2020-06-06 16:30:44 +0300

    ft(login): add login component
    add route to login component in routes file
    create login component in app/components/auth

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from '../components/landing/Landing';
+import Login from '../components/auth/login/Login';
 
 const Routes = () => (
         <Switch>
             <Route path='/' exact component={Landing}/>
+            <Route path='/login' exact component={Login} />
         </Switch>
 );
 
